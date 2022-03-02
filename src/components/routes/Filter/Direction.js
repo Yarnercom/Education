d51b50198c372eecbeac96325c64be00ef873e77
ACER <a.doroev@mail.ru>
2022-03-02 22:23:59 +0600

    update1

--- src/components/routes/Filter/Direction.js
+++ src/components/routes/Filter/Direction.js
@@ -32,25 +32,62 @@ const Direction = () => {
                                     </NavLink>
                                 </ul>
                                 <div className='line'> </div>
-                        <form className='direction__form'>
-                            <label className='direction__label'>
-                                <img className='direction__search' src={Search} alt="search"/>
-                                <input className='direction__input' placeholder='Поиск по направлениям' type="text"/>
-                            </label>
-                            <div className='direction__switch'>
-                                <input className="switch" id="switch1" type="checkbox"/>
-                                <label className="switch-for" htmlFor="switch1">Онлайн-защита</label>
-                            </div>
-                            <div className='direction__switch'>
-                                <input className="switch" id="switch2" type="checkbox"/>
-                                <label className="switch-for1" htmlFor="switch2">Рассрочка</label>
-                            </div>
-                            <div className='direction__switch'>
-                                <input className="switch" id="switch3" type="checkbox"/>
-                                <label className="switch-for2" htmlFor="switch3">Можно перевестись</label>
-                            </div>
-                        </form>
+                       <div className='direction__box1'>
+                           <form className='direction__form'>
+                               <label className='direction__label'>
+                                   <img className='direction__search' src={Search} alt="search"/>
+                                   <input className='direction__input' placeholder='Поиск по направлениям' type="text"/>
+                               </label>
+                               <div className='direction__switch'>
+                                   <input className="switch" id="switch1" type="checkbox"/>
+                                   <label className="switch-for" htmlFor="switch1">Онлайн-защита</label>
+                               </div>
+                               <div className='direction__switch'>
+                                   <input className="switch" id="switch2" type="checkbox"/>
+                                   <label className="switch-for1" htmlFor="switch2">Рассрочка</label>
+                               </div>
+                               <div className='direction__switch'>
+                                   <input className="switch" id="switch3" type="checkbox"/>
+                                   <label className="switch-for2" htmlFor="switch3">Можно перевестись</label>
+                               </div>
+                           </form>
 
+                           <div className='direction__levelEducation'>
+                               <h2>Уровень образования</h2>
+                               <form>
+                                   <label className='direction__checkbox'>
+                                       <input className='direction__customCheckbox' type="checkbox"/>
+                                       <p className='direction__level'>Магистратура</p>
+                                   </label>
+                                   <label className='direction__checkbox'>
+                                       <input className='direction__customCheckbox' type="checkbox"/>
+                                       <p className='direction__level'>Бакалавриат</p>
+                                   </label>
+                                   <label className='direction__checkbox'>
+                                       <input className='direction__customCheckbox' type="checkbox"/>
+                                       <p className='direction__level'>Специалитет</p>
+                                   </label>
+                               </form>
+                           </div>
+
+                           <div className='direction__themeEducation'>
+                               <h2>Тематика направления</h2>
+                               <form>
+                                   <label className='direction__checkbox'>
+                                       <input className='direction__customCheckbox' type="checkbox"/>
+                                       <p className='direction__level'>Гуманитарные</p>
+                                   </label>
+                                   <label className='direction__checkbox'>
+                                       <input className='direction__customCheckbox' type="checkbox"/>
+                                       <p className='direction__level'>Технические</p>
+                                   </label>
+                                   <label className='direction__checkbox'>
+                                       <input className='direction__customCheckbox' type="checkbox"/>
+                                       <p className='direction__level'>Педагогические</p>
+                                   </label>
+                               </form>
+                           </div>
+                       </div>
                     </div>
                 </div>
             </div>

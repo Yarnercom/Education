283bfbfc0fee307aa99fea65f6a6fa90920bbdcb
ACER <a.doroev@mail.ru>
2022-03-10 20:03:53 +0600

    education fix3

--- src/components/routes/Filter/Direction.js
+++ src/components/routes/Filter/Direction.js
@@ -10,7 +10,7 @@ import Economy from "../../../assets/direction/экономика.svg";
 
 const Direction = () => {
 
-    const [searchShow, setSearchShow] = useState(false)
+    const [searchShow, setSearchShow] = useState(false);
 
     const [all, setAll] = useState([]);
 
@@ -32,113 +32,115 @@ const Direction = () => {
                         <p className='direction__subtitle'>Мы сотрудничаем с 10 престижными колледжами по программам дистанционного онлайн-обучения.</p>
 
 
-                               <div className='direction__filter'>
-                                   <ul className='direction__navbar'>
-                                       <NavLink to='/filter' className='direction__navbar-list'>
-                                           Высшее<sup className='sup'>14</sup>
-                                       </NavLink>
-                                       <NavLink to='/filter' className='direction__navbar-list'>
-                                           Среднее <sup className='sup'>9</sup>
-                                       </NavLink>
-                                       <NavLink to='/filter' className='direction__navbar-list'>
-                                           ДПО <sup className='sup'>36</sup>
-                                       </NavLink>
-                                   </ul>
-                                   <div className='line'> </div>
-                                   <div className='direction__box1'>
-                                       <form className='direction__form'>
-                                           <label className='direction__label'>
-                                               <img className='direction__search' src={Search} alt="search"/>
-                                               <input className='direction__input' placeholder='Поиск по направлениям'
-                                                      type="text"
-                                                      onChange={(e)=> search(e.target.value)}
-                                               />
-                                           </label>
-                                           <div className='direction__switch'>
-                                               <input className="switch" id="switch1" type="checkbox"/>
-                                               <label className="switch-for" htmlFor="switch1">Онлайн-защита</label>
-                                           </div>
-                                           <div className='direction__switch'>
-                                               <input className="switch" id="switch2" type="checkbox"/>
-                                               <label className="switch-for1" htmlFor="switch2">Рассрочка</label>
-                                           </div>
-                                           <div className='direction__switch'>
-                                               <input className="switch" id="switch3" type="checkbox"/>
-                                               <label className="switch-for2" htmlFor="switch3">Можно
-                                                   перевестись</label>
-                                           </div>
-                                       </form>
+                              <div className='direction__flex'>
+                                  <div className='direction__filter'>
+                                      <ul className='direction__navbar'>
+                                          <NavLink to='/filter' className='direction__navbar-list'>
+                                              Высшее<sup className='sup'>14</sup>
+                                          </NavLink>
+                                          <NavLink to='/filter' className='direction__navbar-list'>
+                                              Среднее <sup className='sup'>9</sup>
+                                          </NavLink>
+                                          <NavLink to='/filter' className='direction__navbar-list'>
+                                              ДПО <sup className='sup'>36</sup>
+                                          </NavLink>
+                                      </ul>
+                                      <div className='line'> </div>
+                                      <div className='direction__box1'>
+                                          <form className='direction__form'>
+                                              <label className='direction__label'>
+                                                  <img className='direction__search' src={Search} alt="search"/>
+                                                  <input className='direction__input' placeholder='Поиск по направлениям'
+                                                         type="text"
+                                                         onChange={(e)=> search(e.target.value)}
+                                                  />
+                                              </label>
+                                              <div className='direction__switch'>
+                                                  <input className="switch" id="switch1" type="checkbox"/>
+                                                  <label className="switch-for" htmlFor="switch1">Онлайн-защита</label>
+                                              </div>
+                                              <div className='direction__switch'>
+                                                  <input className="switch" id="switch2" type="checkbox"/>
+                                                  <label className="switch-for1" htmlFor="switch2">Рассрочка</label>
+                                              </div>
+                                              <div className='direction__switch'>
+                                                  <input className="switch" id="switch3" type="checkbox"/>
+                                                  <label className="switch-for2" htmlFor="switch3">Можно
+                                                      перевестись</label>
+                                              </div>
+                                          </form>
 
-                                       <div className='direction__levelEducation'>
-                                           <h2>Уровень образования</h2>
-                                           <form>
-                                               <label className='direction__checkbox'>
-                                                   <input className='direction__customCheckbox' type="checkbox"/>
-                                                   <p className='direction__level'>Магистратура</p>
-                                               </label>
-                                               <label className='direction__checkbox'>
-                                                   <input className='direction__customCheckbox' type="checkbox"/>
-                                                   <p className='direction__level'>Бакалавриат</p>
-                                               </label>
-                                               <label className='direction__checkbox'>
-                                                   <input className='direction__customCheckbox' type="checkbox"/>
-                                                   <p className='direction__level'>Специалитет</p>
-                                               </label>
-                                           </form>
-                                       </div>
+                                          <div className='direction__levelEducation'>
+                                              <h2>Уровень образования</h2>
+                                              <form>
+                                                  <label className='direction__checkbox'>
+                                                      <input className='direction__customCheckbox' type="checkbox"/>
+                                                      <p className='direction__level'>Магистратура</p>
+                                                  </label>
+                                                  <label className='direction__checkbox'>
+                                                      <input className='direction__customCheckbox' type="checkbox"/>
+                                                      <p className='direction__level'>Бакалавриат</p>
+                                                  </label>
+                                                  <label className='direction__checkbox'>
+                                                      <input className='direction__customCheckbox' type="checkbox"/>
+                                                      <p className='direction__level'>Специалитет</p>
+                                                  </label>
+                                              </form>
+                                          </div>
 
-                                       <div className='direction__themeEducation'>
-                                           <h2>Тематика направления</h2>
-                                           <form>
-                                               <label className='direction__checkbox'>
-                                                   <input className='direction__customCheckbox' type="checkbox"/>
-                                                   <p className='direction__level'>Гуманитарные</p>
-                                               </label>
-                                               <label className='direction__checkbox'>
-                                                   <input className='direction__customCheckbox' type="checkbox"/>
-                                                   <p className='direction__level'>Технические</p>
-                                               </label>
-                                               <label className='direction__checkbox'>
-                                                   <input className='direction__customCheckbox' type="checkbox"/>
-                                                   <p className='direction__level'>Педагогические</p>
-                                               </label>
-                                           </form>
-                                       </div>
-                                       <div className='direction__duration'>
-                                           <form>
-                                               <h2>Длительность обучения</h2>
-                                               <label className='slider'>
-                                                   <p>1 год — 5 лет</p>
-                                                   <output htmlFor='fader' id='volume'>0</output>
-                                                   <input type="range" id='fader' min='0' max='500' step='1'/>
-                                               </label>
-                                               <h2>Стоимость семестра *</h2>
-                                               <label>
-                                                   <p>0 ₽ — 200 000 ₽</p>
-                                                   <input type="range"/>
-                                               </label>
-                                           </form>
-                                           <p>* Семестр — это 6 месяцев, полугодие</p>
-                                       </div>
-                                   </div>
-                               </div>
-                        <div className='direction__content'>
-                            <ul className='direction__content-card'>
-                                {
-                                    all.map((item, idx)=>(
-                                        <li key={idx} className='direction__content-card_item'>
-                                            <div className='directions__cards-list economy'>
-                                                <p className='directions__cards-title'>{item.title}</p>
-                                                <p className='directions__cards-subtitle'>• от {item.price} ₽ семестр</p>
-                                                <p className='directions__cards-subtitle'>• от 2,5 лет</p>
-                                                <img className='directions__cards-img' src={Economy} alt="Economy"/>
-                                                <button type='button' className='directions__cards-btn'><span>...</span>консультация</button>
-                                            </div>
-                                        </li>
-                                    ))
-                                }
-                            </ul>
-                        </div>
+                                          <div className='direction__themeEducation'>
+                                              <h2>Тематика направления</h2>
+                                              <form>
+                                                  <label className='direction__checkbox'>
+                                                      <input className='direction__customCheckbox' type="checkbox"/>
+                                                      <p className='direction__level'>Гуманитарные</p>
+                                                  </label>
+                                                  <label className='direction__checkbox'>
+                                                      <input className='direction__customCheckbox' type="checkbox"/>
+                                                      <p className='direction__level'>Технические</p>
+                                                  </label>
+                                                  <label className='direction__checkbox'>
+                                                      <input className='direction__customCheckbox' type="checkbox"/>
+                                                      <p className='direction__level'>Педагогические</p>
+                                                  </label>
+                                              </form>
+                                          </div>
+                                          <div className='direction__duration'>
+                                              <form>
+                                                  <h2>Длительность обучения</h2>
+                                                  <label className='slider'>
+                                                      <p>1 год — 5 лет</p>
+                                                      <output htmlFor='fader' id='volume'>0</output>
+                                                      <input type="range" id='fader' min='0' max='500' step='1'/>
+                                                  </label>
+                                                  <h2>Стоимость семестра *</h2>
+                                                  <label>
+                                                      <p>0 ₽ — 200 000 ₽</p>
+                                                      <input type="range"/>
+                                                  </label>
+                                              </form>
+                                              <p>* Семестр — это 6 месяцев, полугодие</p>
+                                          </div>
+                                      </div>
+                                  </div>
+                                  <div className='direction__content'>
+                                      <ul className='direction__content-card'>
+                                          {
+                                              all.map((item, idx)=>(
+                                                  <li key={idx} className='direction__content-card_item'>
+                                                      <div className='directions__cards-list economy'>
+                                                          <p className='directions__cards-title'>{item.title}</p>
+                                                          <p className='directions__cards-subtitle'>• от {item.price} ₽ семестр</p>
+                                                          <p className='directions__cards-subtitle'>• от 2,5 лет</p>
+                                                          <img className='directions__cards-img' src={Economy} alt="Economy"/>
+                                                          <button type='button' className='directions__cards-btn'><span>...</span>консультация</button>
+                                                      </div>
+                                                  </li>
+                                              ))
+                                          }
+                                      </ul>
+                                  </div>
+                              </div>
                     </div>
                 </div>
             </div>
